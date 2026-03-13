404c4ac984e608c2082c3dfe7f17c78db878e9eb
Ashmeet Singh <ashmeet@example.com>
2026-03-13 18:12:32 +0530

    added footer

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 /* BACKGROUND IMAGES */
 import jsBg from "../photos/js.jpg";
@@ -293,6 +294,7 @@ function CategoryPage() {
           </div>
         </div>
       </div>
+       <Footer />
     </>
   );
 }
