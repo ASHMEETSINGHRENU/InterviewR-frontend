11000427dc2ba6f17d329464125d5ebfc22a8e62
Ashmeet Singh <ashmeet@example.com>
2026-03-13 18:01:47 +0530

    update code

--- src/pages/AdminUpload.js
+++ src/pages/AdminUpload.js
@@ -134,7 +134,7 @@ className="w-full border p-3 rounded"
 <option value="dsa">DSA</option>
 <option value="ml">ML</option>
 <option value="python">Python</option>
-<option value="human_resources">HR</option>
+<option value="HR">HR</option>
 <option value="AI">AI</option>
 
 </select>
@@ -179,7 +179,7 @@ Manage Questions
 <button onClick={()=>loadQuestions("mongodb")} className="bg-blue-600 text-white p-2 rounded">MongoDB</button>
 <button onClick={()=>loadQuestions("mysql")} className="bg-blue-600 text-white p-2 rounded">MySQL</button>
 <button onClick={()=>loadQuestions("python")} className="bg-blue-600 text-white p-2 rounded">Python</button>
-<button onClick={()=>loadQuestions("human_resources")} className="bg-blue-600 text-white p-2 rounded">HR</button>
+<button onClick={()=>loadQuestions("HR")} className="bg-blue-600 text-white p-2 rounded">HR</button>
 <button onClick={()=>loadQuestions("html")} className="bg-blue-600 text-white p-2 rounded">HTML</button>
 <button onClick={()=>loadQuestions("css")} className="bg-blue-600 text-white p-2 rounded">CSS</button>
 <button onClick={()=>loadQuestions("ml")} className="bg-blue-600 text-white p-2 rounded">ML</button>
